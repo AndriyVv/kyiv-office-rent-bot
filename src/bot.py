20a3adc651951e4e4770f37cd16f7130d55ff29f
alina.potapenko <alina.potapenko@gcpsoftware.com>
2025-04-11 11:39:49 +0300

    change found offers view

--- src/bot.py
+++ src/bot.py
@@ -9,24 +9,21 @@ from aiogram.filters import Command
 from aiogram import F
 from aiogram import Router
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.client.default import DefaultBotProperties
 
 API_TOKEN = os.environ.get('API_TOKEN')
 TELEGRAM_API_ID = os.environ.get('TELEGRAM_API_ID')
 TELEGRAM_API_HASH = os.environ.get('TELEGRAM_API_HASH')
 CHANNEL_USERNAME = '@KyivOfficeRent'
 
-# Logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-# Client initialization
 router = Router(name=__name__)
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 dp = Dispatcher()
 dp.include_router(router)
 telethon_client = TelegramClient('user_session', TELEGRAM_API_ID, TELEGRAM_API_HASH)
-
 
 async def fetch_channel_messages():
     async with telethon_client:
@@ -40,9 +37,12 @@ async def fetch_channel_messages():
             logger.info(f"Messages loaded: {len(history)}")
             return history
         except Exception as e:
-            logger.error(f"An error ocurred while receiving messages: {e}")
+            logger.error(f"An error occurred while receiving messages: {e}")
             return []
 
+def extract_metro_station(text):
+    match = re.search(r"\u24C2️([\w\s\-]+)", text)
+    return match.group(1).strip() if match else ""
 
 async def parse_and_filter_messages(messages, min_size, max_size, min_price_per_m2=None, max_price_per_m2=None):
     offers = []
@@ -51,14 +51,14 @@ async def parse_and_filter_messages(messages, min_size, max_size, min_price_per_
             continue
 
         bc_match = re.search(r"Бізнес-(?:центр|парк) ([^\n]+)", message)
-        bc_name = bc_match.group(1) if bc_match else "Неизвестный БЦ"
+        bc_name = bc_match.group(1) if bc_match else "Неизвестний БЦ"
+        metro_station = extract_metro_station(message)
 
         offer_matches = re.findall(
             r"(\d+(?:-й|-й поверх| поверх|й поверх)) (\d+(?:\.\d+)?)m2 \((\d+(?:\.\d+)?\$)\)",
             message
         )
 
-        # Parsing links from the formatted text
         offer_links = {}
         if entities:
             for entity in entities:
@@ -71,11 +71,10 @@ async def parse_and_filter_messages(messages, min_size, max_size, min_price_per_
         for floor, size, price in offer_matches:
             size_number = float(size)
             price_total = float(price[:-1])
-            price_per_m2 = round(price_total / size_number, 2)  # Calculating price for m2
+            price_per_m2 = round(price_total / size_number, 2)
 
             if (min_size <= size_number if max_size is None else min_size <= size_number <= max_size):
                 if (min_price_per_m2 is None or price_per_m2 >= min_price_per_m2) and (max_price_per_m2 is None or price_per_m2 <= max_price_per_m2):
-                    # Generating key for searching for the link
                     offer_key = f"{floor} {size}m2 ({price})"
                     link = None
                     for offer_text, offer_url in offer_links.items():
@@ -85,10 +84,11 @@ async def parse_and_filter_messages(messages, min_size, max_size, min_price_per_
                             break
                     if not link:
                         link = f"https://t.me/{CHANNEL_USERNAME[1:]}/{msg_id}"
-                    offers.append((f"{bc_name}: {floor}, {size}m², {price} ({price_per_m2}$/m²)", link))
-    logger.info(f"Found offers: {len(offers)}")
-    return offers
+                    text = f"<b>{bc_name}</b>\n{floor}, {size}м²\n💵 {price} ({price_per_m2}$/м²)\nⓂ️{metro_station}"
+                    offers.append((price_total, text, link))
 
+    offers.sort(key=lambda x: x[0])
+    return [(text, link) for _, text, link in offers]
 
 @router.message(Command("start"))
 async def start_handler(message: types.Message):
@@ -99,20 +99,15 @@ async def start_handler(message: types.Message):
         [InlineKeyboardButton(text="500-1000 м²", callback_data="size_500_1000")],
         [InlineKeyboardButton(text="1000+ м²", callback_data="size_1000_")]
     ]
-
-    # Adding 'new search' button
     restart_buttons = [
         [KeyboardButton(text="Новий пошук")]
     ]
-
     await message.answer("Оберіть метраж:", reply_markup=InlineKeyboardMarkup(inline_keyboard=buttons))
     await message.answer("Для нового пошуку натисніть кнопку нижче:", reply_markup=ReplyKeyboardMarkup(keyboard=restart_buttons, resize_keyboard=True))
-
 
 @router.message(F.text == "Новий пошук")
 async def restart_handler(message: types.Message):
     await start_handler(message)
-
 
 @router.callback_query(F.data.startswith("size_"))
 async def size_filter_handler(callback_query: types.CallbackQuery):
@@ -125,7 +120,6 @@ async def size_filter_handler(callback_query: types.CallbackQuery):
         [InlineKeyboardButton(text="Високий (більше 30$ за м²)", callback_data=f"price_{min_size}_{max_size}_30_100000")]
     ]
     await callback_query.message.answer("Оберіть діапазон ціни:", reply_markup=InlineKeyboardMarkup(inline_keyboard=buttons))
-
 
 @router.callback_query(F.data.startswith("price_"))
 async def price_filter_handler(callback_query: types.CallbackQuery):
@@ -144,18 +138,16 @@ async def price_filter_handler(callback_query: types.CallbackQuery):
         await callback_query.message.answer("На жаль, відповідних варіантів не знайдено.")
         return
 
-    buttons = []
     for offer_text, link in offers:
         if link:
-            buttons.append([InlineKeyboardButton(text=offer_text, url=link)])
-
-    await callback_query.message.answer("Доступні варіанти:", reply_markup=InlineKeyboardMarkup(row_width=1, inline_keyboard=buttons))
-
+            markup = InlineKeyboardMarkup(
+                inline_keyboard=[[InlineKeyboardButton(text="Перейти ➡️", url=link)]]
+            )
+            await callback_query.message.answer(offer_text, reply_markup=markup)
 
 async def start_telethon():
     await telethon_client.start()
     print(f"Telegram Client {telethon_client.session} is connected!")
-
 
 async def main():
     await start_telethon()
